b888bb55f6b7156cf42ddb07a2ac6828e8e2385c
kko0831 <kko0831@hanmail.net>
2020-08-24 19:26:52 +0900

    2.4.4-4 AND 연산자(&&)를 사용한 조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 function App() {
-  const name = "리액트";
-  return <div>{name === "리액트" && <h1>리액트입니다.</h1>}</div>;
+  const number = 0;
+  return number && <div>내용</div>;
 }
 
 export default App;
